1b36f412a2108a834ac5703b57125df1ab727b3d
Andrew Rodriguez <andrewrodriguez@fullsail.edu>
2014-07-24 22:57:14 -0400

    Update Rodriguez_Andrew_Assignment3.js

--- Rodriguez_Andrew_Assignment3.js
+++ Rodriguez_Andrew_Assignment3.js
@@ -1,1 +1,9 @@
-alert("JavaScript works!");
+// alert("JavaScript works!");
+
+// Andrew Rodriguez
+// SDI - 1407
+// Assignement 1 Project 1
+// JavaScript - FlowChart
+// "wolfs" "Hunting" "weapons"
+
+// Preparing to Hunt some wolfs! Deciding what kind of protection to wear and weapon to use.
