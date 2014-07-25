5b44d416b8ace2f087d977d439a2b1873945e776
Andrew Rodriguez <andrewrodriguez@fullsail.edu>
2014-07-24 22:57:55 -0400

    Update Rodriguez_Andrew_Assignment3.js

--- Rodriguez_Andrew_Assignment3.js
+++ Rodriguez_Andrew_Assignment3.js
@@ -7,3 +7,26 @@
 // "wolfs" "Hunting" "weapons"
 
 // Preparing to Hunt some wolfs! Deciding what kind of protection to wear and weapon to use.
+
+// Variables
+
+var myname = "Andrew";
+var protection1 = "Long harden sleeves";
+var protection2 = "face mask";
+var weapon1 = "hunting rifle";
+var weapon2 = "Bow and arrow";
+var numberofwolfs = 1;
+var huntingawolf = true;
+var myprompt;
+
+// Output
+
+console.log("afternoon everyone, my name is" ,myname, "Arod for short.");
+console.log("this afternoon I will be going hunting for" ,numberofwolfs, "wolf");
+console.log("first I will need to decide what kind of protection I will be using");
+console.log("if I use" ,protection1, "for protection my choice of weapon is a" ,weapon2, ".");
+console.log("the reason for this combination is I need something to protect my forearms from the bowstring, this is also good protection against close combat with a wolf in the event it was trying to bite my arms.");
+console.log("if I decide to go with" ,protection2, "as protection my choice of weapon would be" ,weapon1, ".");
+console.log("the" ,protection2, "is to protect my face and eyes because I would be using a" ,weapon1, "to hunt the wolf.");
+console.log("the" ,weapon2, "makes good for long distances and can be just as accurte as an" ,weapon1, "is.");
+console.log("Time to pick my combination of protection and weapon.");
